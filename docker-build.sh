789b00a67a2077d1b1b532e5448e438deea4b5a8
nodehome-project <nodehome@4intel.net>
2019-04-24 12:13:05 +0900

    setup

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,14 @@
 #/bin/bash
 
+# docker-runner-script
+mkdir service-runner
+mkdir service-runner/config
+cp -r ./docker-compose.yaml ./service-runner/docker-compose.yaml
+cp -r ./docker-run.sh ./service-runner/docker-run.sh
+cp -r ./tomcat ./service-runner/config/tomcat
+cp -r ./source/WEB-INF/hosts ./service-runner/config/hosts
+cp -r ./source/WEB-INF/classes/resources/props/globals.properties ./service-runner/config/globals.properties
+
 #ex) docker rmi test-service:0.1
 docker rmi {docker account}/{image name}:0.1
 #docker build -t test-service:0.1 .
