bdda17a2adaacffe92a39ef09eccd6591225853b
nodehome-project <48472782+nodehome-project@users.noreply.github.com>
2019-08-22 14:35:41 +0900

    Update run.sh

--- nodem/run.sh
+++ nodem/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 Ans=`curl --silent http://127.0.0.1:8886/version.bin | awk '/\"OK\"/{print "OK"}'`
 if [ "$Ans" == "OK" ]; then
-  curl --silent http://127.0.0.1:8886/exit.bin?key=fouri
+  curl --silent http://127.0.0.1:8886/exit.bin?key=nodehome
   # wait for teminate completely
   sleep 3
 fi
@@ -9,5 +9,6 @@ fi
 LogFolder=/home/nodehome/nodem/log
 LogLink=$LogFolder"/nodem.TRACE"
 
-/home/nodehome/nodem/nodem -h -e -v -etime 600 -htime 300 -flog  &
+#/home/nodehome/nodem/nodem -h -e -v -etime 600 -htime 300 -flog  &
+/home/nodehome/nodem/nodem -v -etime 600 -htime 300 -flog -x nodehome
 # tail -f $LogLink
